584066fc376afe9350bc31d7786635b8bfcbd951
Ertas10 <nuno10_rayman.crash@live.com.pt>
2018-12-13 09:57:25 +0000

    A

--- TankGame/CollisionManager.cs
+++ TankGame/CollisionManager.cs
@@ -23,8 +23,8 @@ namespace TankGame
                 float tangent = (float)(Math.Sqrt(Math.Pow(tank1.pos.X - tank2.pos.X, 2f)) / Math.Sqrt(Math.Pow(tank1.pos.Z - tank2.pos.Z, 2f)));
                 float angle1 = MathHelper.ToDegrees((float)Math.Atan(tangent));
                 float hypotenuse = (float)Math.Sqrt(Math.Pow(Vector3.Distance(tank1.pos, tank2.pos) - (tank1.colRadius + tank2.colRadius), 2));
-                float movX = (float)(Math.Sin(angle1) * hypotenuse) / 2f;
-                float movZ = (float)(Math.Cos(angle1) * hypotenuse) / 2f;
+                float movX = (float)(Math.Sin(angle1) * hypotenuse);
+                float movZ = (float)(Math.Cos(angle1) * hypotenuse);
                 if (tank1.pos.X < tank2.pos.X)
                 {
                     tank1.pos.X -= movX;
